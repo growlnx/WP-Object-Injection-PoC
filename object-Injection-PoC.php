06f5e234e16820d0eb47d1b02d1fcca6ee6c20e9
growlnx <growlnx@protonmail.com>
2020-09-07 23:51:33 -0300

    add magic methods

---
+++ object-Injection-PoC.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @package WP-Object-Injection-PoC
+ * @version 1.0
+ */
+
+/*
+Plugin Name: WP Object Injection Proof of Concept
+Plugin URI: https://github.com/growlnx/WP-Object-Injection-PoC
+Description: This is a simple plugin to make PoC of PHP Object Injection in Wordpress.
+Author: Growlnx
+Version: 1.0
+Author URI: https://github.com/growlnx/
+*/
+
+class ObjectInjection
+{
+
+  private $cmd = "";
+  private $fcn = "";
+
+  function notEmptyArgs() {
+    if($this->cmd != "" and $this->fcn != "") {
+      return false;
+    }
+    return true;
+  }
+
+  function __destruct() {
+    if($this->notEmptyArgs()) {
+     $this->fcn($this->cmd);
+    }
+  }
+
+  function __wakeup() {
+    if($this->cmd != "") {
+      $this->fcn($this->cmd);
+    }
+  }
+
+};
